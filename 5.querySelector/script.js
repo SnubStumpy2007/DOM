4efa588f6eff2bc760caa064836bb5020c8c7d3c
SnubStumpy2007 <joshua192007@gmail.com>
2024-02-29 14:46:21 -0500

    update

---
+++ 5.querySelector/script.js
@@ -0,0 +1,14 @@
+// 1. fetch first product card
+const firstProductCard = document.querySelector(".product-card")
+console.log(firstProductCard)
+
+const highestRating = document.querySelector("[data-rating='4.8']")
+console.log(highestRating)
+
+// first laptop product
+const laptop = document.querySelector("[data-category='laptop']")
+console.log(laptop)
+
+// selecting an id
+const firstProductEl = document.querySelector("#dashboard")
+console.log(firstProductEl)
