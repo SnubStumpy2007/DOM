f9e26938fc5aa964063b8a02c3a0e4204c7124ae
SnubStumpy2007 <joshua192007@gmail.com>
2024-02-29 13:53:39 -0500

    update

--- 2.Window-Object/script.js
+++ 2.Window-Object/script.js
@@ -1,5 +1,7 @@
 // window object
-console.log(window.location.host)
-console.log(window.screen)
-console.log(window.innerWidth)
-console.log(window.navigator)
+// console.log(window.location.host)
+// console.log(window.screen)
+// console.log(window.innerWidth)
+// console.log(window.navigator)
+
+console.log(window)
