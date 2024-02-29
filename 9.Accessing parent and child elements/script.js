3db61e5db1f2245546260ac6315dac3c8a4cabe3
SnubStumpy2007 <joshua192007@gmail.com>
2024-02-29 16:07:52 -0500

    update

---
+++ 9.Accessing parent and child elements/script.js
@@ -0,0 +1,7 @@
+// accessing parent element
+const parentEL = document.getElementById("parent")
+console.log(parentEL.parentNode)
+
+// child element access
+const firstChildEl = parentEL.lastElementChild
+console.log(firstChildEl)
