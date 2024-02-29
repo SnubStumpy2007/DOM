1c815cf9a156fe1dea6056495fd927193a3fc106
SnubStumpy2007 <joshua192007@gmail.com>
2024-02-29 14:04:23 -0500

    update

---
+++ 3.getElementById/script.js
@@ -0,0 +1,6 @@
+// selecting elements by ID
+const title = document.getElementById("conferenceTitle")
+console.log(title)
+
+const conferenceLocation = document.getElementById("conferenceLocation")
+console.log(conferenceLocation)
